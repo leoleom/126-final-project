4eb1a059e3144f2d45530e35345575e18e8c8ca7
Junel-A <junelarellano17@gmail.com>
2026-05-08 02:47:55 +0800

    feat: add skeleton action listeners to main navbar

--- src/frontend/components/navbar.jsx
+++ src/frontend/components/navbar.jsx
@@ -3,6 +3,19 @@ import { Link, useLocation } from "react-router-dom";
 function Navbar({user}) {
   const location = useLocation();
 
+  // --- SKELETON ACTION LISTENERS ---
+  const handleNavClick = (label) => {
+    console.log(`${label} navigation link clicked`);
+  };
+
+  const handleCreatePostClick = () => {
+    console.log("Create Post button clicked");
+  };
+
+  const handleLogoutClick = () => {
+    console.log("Logout link clicked");
+  };
+
   const navItems = [
     { label: "Home", path: "/feed" },
     { label: "Profile", path: "/profile" },
@@ -22,8 +35,10 @@ function Navbar({user}) {
         Better Better UPV
       </h1>
 
+      {/* Create Post Button */}
       <Link
         to="/create-post"
+        onClick={handleCreatePostClick} // Added listener
         className="mt-16 flex h-14 w-full items-center justify-center rounded-lg bg-[#3f6f4f] text-sm font-extrabold text-white"
       >
         + Create Post
@@ -37,6 +52,7 @@ function Navbar({user}) {
             <Link
               key={item.label}
               to={item.path}
+              onClick={() => handleNavClick(item.label)} // Added listener
               className={`flex h-12 items-center rounded-lg px-7 ${
                 isActive
                   ? "bg-[#e6f0ea] text-[#111827]"
@@ -49,7 +65,12 @@ function Navbar({user}) {
         })}
       </nav>
 
-      <Link to="/" className="mt-28 block px-7 text-sm font-extrabold">
+      {/* Logout Link */}
+      <Link 
+        to="/" 
+        onClick={handleLogoutClick} // Added listener
+        className="mt-28 block px-7 text-sm font-extrabold"
+      >
         Logout
       </Link>
     </aside>
