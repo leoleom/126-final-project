164364db6f03c7eb1f2c28eb07534dbef4b59196
G4l3S <gabs.sumergido@gmail.com>
2026-05-15 02:54:01 +0800

    refactor: connection to db

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,12 +1,19 @@
-require('dotenv').config();
+require('dotenv').config({
+  path: '../../.env'
+});
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
--- src/backend/config/supabaseClient.js
+++ src/backend/config/supabaseClient.js
@@ -1,8 +1,13 @@
 const { createClient } = require('@supabase/supabase-js');
 
+console.log("SUPABASE URL:", process.env.SUPABASE_URL);
+console.log(
+  "SECRET KEY EXISTS:",
+  !!process.env.SUPABASE_SECRET_KEY
+);
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_SECRET_KEY
 );
 
 module.exports = supabase;
